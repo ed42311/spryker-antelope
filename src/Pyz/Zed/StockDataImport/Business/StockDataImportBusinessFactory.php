4e897ff8910daa7fd0f703557afb330e749654dd
Edward Weymouth <edward.weymouth@murdochs.com>
2021-03-15 17:40:35 -0600

    [update] Add business Factory

---
+++ src/Pyz/Zed/StockDataImport/Business/StockDataImportBusinessFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Zed\StockDataImport\Business;
+
+use Spryker\Zed\StockDataImport\Business\StockDataImportBusinessFactory as SprykerStockDataImportBusinessFactory;
+
+/**
+ * @method \Spryker\Zed\StockDataImport\StockDataImportConfig getConfig()
+ */
+class StockDataImportBusinessFactory extends SprykerStockDataImportBusinessFactory
+{
+}
